b7d2d2fa74b5028b467f9d17a73cb7acbfb76023
R0bfried <burgharo@hs-pforzheim.de>
2024-06-16 13:26:24 +0000

    Für Niki

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,23 @@ import streamlit as st
 from elevenlabs import play, stream
 from elevenlabs.client import ElevenLabs
 import os
-import ffmpeg
-os.environ['OPENAI_API_KEY'] = "sk-proj-NLve14xoGJ5yhdxGnwZAT3BlbkFJpd9Qyu4c3to7UwY0yaZl"
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+import ffmpeg
+
+os.environ['OPENAI_API_KEY'] = 'sk-proj-2NIomBT1UrHqQqvDMcMST3BlbkFJOApKYaxtgphQdxu9qsx1'
 client = ElevenLabs(
-    api_key="1c926715df7d2788a147631eeafa2e8d"
-)
+    api_key= '1c926715df7d2788a147631eeafa2e8d'
+    )
+
+
+#with st.form('inputs'):
+#    OAI_Key = st.text_input('OpenAI-Key', 'Hier OPEN-AI API Key einfuegen')
+#    EL_Key = st.text_input('Elevenlabs-Key','Hier Elevenlabs API Key einfuegen')
+#    submitAPI = st.form_submit_button ('OK')
+#    if submitAPI:
+
+
+
 st.title('MEM-Bot')
 with st.form('my form'):
     text = st.text_area('Enter text:', 'Hier Anfrage zum MEM-Studiengang stellen')
@@ -16,15 +27,15 @@ with st.form('my form'):
         documents = SimpleDirectoryReader("data").load_data()
         index = VectorStoreIndex.from_documents(documents)
         query_engine = index.as_query_engine()
-        response = query_engine.query(text)
-        st.text_area('Output:', response)
+        response = query_engine.query("What is expected from the students?")
+        print(response)
+
         audio = client.generate(
             text=str(response),
             voice = "Rachel",
             model = "eleven_multilingual_v2",
         )
-        stream (audio)
-
+        play (audio)
 # Welcome to Streamlit!
 
 #Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
