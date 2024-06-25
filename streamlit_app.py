52425a8e1a10f088bd1a5d7994cfd9001dcce6bc
R0bfried <burgharo@hs-pforzheim.de>
2024-06-25 10:59:39 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ def initialize_chat():
 
 # Initialize chat history
 if "messages" not in st.session_state:
-    initialize_chat
+    st.session_state.messages = []
 
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
